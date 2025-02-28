b878be3b989b8787a9cc709cb7270708a155909a
h2danilo <h2danilo@hotmail.com>
2025-02-27 21:13:15 -0300

    feat: retorna idade

--- src/main/java/com/fatec/projeto/projeto2025/controllers/ExercicioController.java
+++ src/main/java/com/fatec/projeto/projeto2025/controllers/ExercicioController.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -21,5 +23,26 @@ public class ExercicioController {
     public String HelloWorld(@PathVariable Optional<String> nome) {
         return nome.isPresent() ? nome.get() : "dd";
     }
-    
+   
+    @GetMapping("/get-idade/{idade}")
+    public String RetornaIdade(@PathVariable Integer idade) {
+        try {
+         if (idade < 0) {
+            throw new NumberFormatException();
+         }
+
+         if (idade < 12 ) {
+            return "Crianca";
+         } else if (idade <= 18) {
+            return "Adolescente";
+         } else if (idade <= 60) {
+            return "Adulto";
+         } else {
+            return "Idoso";
+         }
+            
+        } catch (NumberFormatException e) {
+            return "idade invalida";
+        }
+    }
 }
